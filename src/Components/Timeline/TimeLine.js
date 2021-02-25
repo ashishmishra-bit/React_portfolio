dab9b64029d9914466667589808e8624c9e839c6
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-02-25 18:02:05 +0530

    timeline porps

--- src/Components/Timeline/TimeLine.js
+++ src/Components/Timeline/TimeLine.js
@@ -1,38 +1,41 @@
-import React from 'react'
-import Timeline from '@material-ui/lab/Timeline';
-import TimelineItem from '@material-ui/lab/TimelineItem';
-import TimelineSeparator from '@material-ui/lab/TimelineSeparator';
-import TimelineConnector from '@material-ui/lab/TimelineConnector';
-import TimelineContent from '@material-ui/lab/TimelineContent';
-import TimelineDot from '@material-ui/lab/TimelineDot';
+import React from "react";
+import { Typography } from '@material-ui/core';
+import Timeline from "@material-ui/lab/Timeline";
+import TimelineItem from "@material-ui/lab/TimelineItem";
+import TimelineSeparator from "@material-ui/lab/TimelineSeparator";
+import TimelineConnector from "@material-ui/lab/TimelineConnector";
+import TimelineContent from "@material-ui/lab/TimelineContent";
+import TimelineDot from "@material-ui/lab/TimelineDot";
 
-import './Timeline.css';
+import "./Timeline.css";
 
-const TimeLine = () => {
+
+const TimeLine = ({title}) => {
     return (
-        <Timeline>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>Eat</TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot />
-            <TimelineConnector />
-          </TimelineSeparator>
-          <TimelineContent>Code</TimelineContent>
-        </TimelineItem>
-        <TimelineItem>
-          <TimelineSeparator>
-            <TimelineDot />
-          </TimelineSeparator>
-          <TimelineContent>Sleep</TimelineContent>
-        </TimelineItem>
-      </Timeline>
-    )
-}
+        <Timeline className={'timeline'}>
+            <TimelineItem className={'timeline_firstItem'}>
+                <TimelineSeparator>
+                    <TimelineDot className={'timeline_dots_header'} />
+                    <TimelineConnector />
+                </TimelineSeparator>
+                <TimelineContent><Typography variant='h6' className={'timeline_header'}>{title}</Typography></TimelineContent>
+            </TimelineItem>
 
-export default TimeLine
+
+            <TimelineItem>
+                <TimeLineSeparator />
+                <TimelineContent>Code</TimelineContent>
+            </TimelineItem>
+          
+        </Timeline>
+    );
+};
+
+export const TimeLineSeparator = () =>(
+    <TimelineSeparator className={'separator_padding'}>
+                    <TimelineDot className={'timeline_dot'} />
+                    <TimelineConnector />
+                </TimelineSeparator>
+);
+
+export default TimeLine;
