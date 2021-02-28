c60b1a713561220d279edf26d3abe17dde956962
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-02-28 19:44:56 +0530

    jason

--- src/utils/resumeDetails.js
+++ src/utils/resumeDetails.js
@@ -1,4 +1,9 @@
 export default {
     name : 'Ashih Kumar Mishra',
-    title : 'Full Stack Web Developer & App Developer'
+    title : 'Full Stack Web Developer & Android Developer',
+
+    jobs : 'Student',
+    email : 'ashish.mishra52002@gmail.com',
+    phone : '+91-8822161061',
+    address : 'Guwahati , Assam'
 }
