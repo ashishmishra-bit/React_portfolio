8a02ffadeea9ec900c4b174d13a7c48e7c36af5c
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-02-28 19:52:58 +0530

    jason

--- src/utils/resumeDetails.js
+++ src/utils/resumeDetails.js
@@ -1,3 +1,8 @@
+import FacebookIcon from '@material-ui/icons/Facebook';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import InstagramIcon from '@material-ui/icons/Instagram';
+
 export default {
     name : 'Ashih Kumar Mishra',
     title : 'Full Stack Web Developer & Android Developer',
@@ -5,5 +10,28 @@ export default {
     jobs : 'Student',
     email : 'ashish.mishra52002@gmail.com',
     phone : '+91-8822161061',
-    address : 'Guwahati , Assam'
+    address : 'Guwahati , Assam',
+
+    social: {
+        facebook:{
+            link : 'https://www.facebook.com',
+            text : 'Ashish',
+            icon : <FacebookIcon />,
+        },     
+        linkedIn:{
+            link : 'https://www.facebook.com',
+            text : 'Ashish',
+            icon : <LinkedInIcon />,
+        },
+        github:{
+            link : 'https://www.facebook.com',
+            text : 'Ashish',
+            icon : <GitHubIcon />,
+        },
+        instagram:{
+            link : 'https://www.facebook.com',
+            text : 'Ashish',
+            icon : <InstagramIcon />,
+        },  
+    }
 }
