611b41cf14ae7624f9597622ef08b0baeda9f15d
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-03-02 13:54:32 +0530

    profile box

--- src/App.js
+++ src/App.js
@@ -1,38 +1,41 @@
-import React from 'react';
-import { Container , Grid } from '@material-ui/core';
+import React from "react";
+import { Container, Grid } from "@material-ui/core";
 
-import Profile from './Components/Profile/Profile'
-import Header from './Components/Header/Header'
-import Footer from './Components/Footer/Footer'
-import Portfolio from './Pages/Portfolio/Portfolio'
-import Resume from './Pages/Resume/Resume'
+import Profile from "./Components/Profile/Profile";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
+import Portfolio from "./Pages/Portfolio/Portfolio";
+import Resume from "./Pages/Resume/Resume";
 
-import {BrowserRouter as Router , Switch ,Route} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
 function App() {
-  return (
-    
-    <Container>
-      <Grid container>
-        <Grid item xs={12} sm={12}  md={4} lg={3}>
-          <Profile />
-        </Grid>         
-        <Grid item xs>
-        <Header />
-       <Router>
-       <Switch>
-       <Route path="/portfolio"><Portfolio /></Route>        
-       <Route path="/"> <Resume /></Route>
-       </Switch>
-       </Router>
-        <Footer />
-        </Grid>
-      </Grid>
-    </Container>
-
-  );
+    return (
+        <Container className={"top_60"}>
+            <Grid container spacing={8}>
+                <Grid item xs={12} sm={12} md={4} lg={3}>
+                    <Profile />
+                </Grid>
+                <Grid item xs>
+                    <Header />
+                    <Router>
+                        <Switch>
+                            <Route path="/portfolio">
+                                <Portfolio />
+                            </Route>
+                            <Route path="/">
+                                {" "}
+                                <Resume />
+                            </Route>
+                        </Switch>
+                    </Router>
+                    <Footer />
+                </Grid>
+            </Grid>
+        </Container>
+    );
 }
 
 export default App;
