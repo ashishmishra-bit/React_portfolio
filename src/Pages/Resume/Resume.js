91a5b6ed0ccf9860ec274065e59c7b68e4bfd677
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-03-04 13:39:56 +0530

    about container

--- src/Pages/Resume/Resume.js
+++ src/Pages/Resume/Resume.js
@@ -6,32 +6,41 @@ import resumeDetails from '../../utils/resumeDetails';
 
 const Resume = () => {
     return (
-       <>
-       {/* about me */} 
-       <Grid container className = 'section pb_45'>
-            <Grid item className = 'section_title mb_30'>
-                <span></span>
-               <h6 className = 'section_title_text'>About Me</h6>
+        <>
+            {/* about me */}
+            <Grid container className='section pb_45'>
+                <Grid item className='section_title mb_20'>
+                    <span></span>
+                    <h6 className='section_title_text'>About Me</h6>
+                </Grid>
+                <Grid item xs={12}>
+                    <Typography variant='body2' className='aboutme_text'>{resumeDetails.about}</Typography>
+                </Grid>
             </Grid>
-       </Grid>
 
-       {/* Education & exp.*/}
-       <Grid container className = 'section' xs={12}>
-       <Typography variant='body2' className = 'aboutme_text'>{resumeDetails.about}</Typography>
-       </Grid>
+            {/* Education & exp.*/}
+            <Grid container className='section' xs={12}>
+                <Grid item className='section_title mb_20'>
+                    <span></span>
+                    <h6 className='section_title_text'>Resume</h6>
+                </Grid>
 
-      {/* Services */}
-       <Grid container className = 'section'></Grid>
+            
 
-       {/* skills*/}
+            </Grid>
 
-       <Grid container className = 'section'></Grid>
+            {/* Services */}
+            <Grid container className='section'></Grid>
 
-       {/* contact */}
-       
-       <Grid container className = 'section'></Grid>
-       
-       </>
+            {/* skills*/}
+
+            <Grid container className='section'></Grid>
+
+            {/* contact */}
+
+            <Grid container className='section'></Grid>
+
+        </>
     );
 };
 
