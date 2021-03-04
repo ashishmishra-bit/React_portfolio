6e9ffe15a48da206ddef56798556b8a6096dd57d
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-03-04 13:18:04 +0530

    about me completed

--- src/Pages/Resume/Resume.js
+++ src/Pages/Resume/Resume.js
@@ -1,26 +1,35 @@
 import React from 'react'
 import './Resume.css';
 import { Container, Grid } from "@material-ui/core";
+import { Typography } from "@material-ui/core";
+import resumeDetails from '../../utils/resumeDetails';
 
 const Resume = () => {
     return (
        <>
-       {/* about me */}
-       <Grid container className = ''></Grid>
+       {/* about me */} 
+       <Grid container className = 'section pb_45'>
+            <Grid item className = 'section_title mb_30'>
+                <span></span>
+               <h6 className = 'section_title_text'>About Me</h6>
+            </Grid>
+       </Grid>
 
        {/* Education & exp.*/}
-       <Grid container className = ''></Grid>
+       <Grid container className = 'section' xs={12}>
+       <Typography variant='body2' className = 'aboutme_text'>{resumeDetails.about}</Typography>
+       </Grid>
 
       {/* Services */}
-       <Grid container className = ''></Grid>
+       <Grid container className = 'section'></Grid>
 
        {/* skills*/}
 
-       <Grid container className = ''></Grid>
+       <Grid container className = 'section'></Grid>
 
        {/* contact */}
        
-       <Grid container className = ''></Grid>
+       <Grid container className = 'section'></Grid>
        
        </>
     );
--- src/utils/resumeDetails.js
+++ src/utils/resumeDetails.js
@@ -37,4 +37,10 @@ export default {
             icon : <TwitterIcon />,
         }, 
     },
+
+    about: 'Hello guys, this is Ashish Kumar Mishra. I am an earnest techie  with keen interest in every new updates that come up    everyday. I love being hardworking and dedicated to whatever I take up as a task. Web development attracts me the most. And I also have some basic idea in android. Apart from that I also have knowledge on SEO.The last one year has been an amazing experience.\n\nThe journey of my BTech years has started with these new experiences and I felt a keen interest growing in me. Getting to learn these things have always been worth it. But this is just the beginning. I am always brushing up my skills more to attain perfection. Looking forward to gather more knowledge and being more accurate in my field.'
+
+    
+
+
 };
